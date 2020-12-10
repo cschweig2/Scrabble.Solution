4537dda7e3130e69b3d73740f5d46651114689a5
Chelsea Becker <cschweig2@gmail.com>
2020-12-10 15:19:37 -0800

    add Dictionary class, remove regex
    Co-authored-by: Agata Kolodziej <agatakolohe@gmail.com>

--- Scrabble.Tests/ModelTests/WordsTest.cs
+++ Scrabble.Tests/ModelTests/WordsTest.cs
@@ -1,1 +1,19 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Scrabble.Models;
+using System.Collections.Generic;
+using System;
+
+namespace Scrabble.Tests
+{
+    [TestClass]
+    public class WordsTests
+    {
+        [TestMethod]
+        public void WordsConstructor_CreatesInstancesOfWords_Words()
+        {
+            Words newWords = new Words();
+            Assert.AreEqual(typeof(Words), newWords.GetType());
+        }
+        
+    }
+}
